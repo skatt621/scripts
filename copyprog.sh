c4927824c722f63f3fed6c5a245a296934024076
skatt621 <37593984+skatt621@users.noreply.github.com>
2020-09-10 23:02:30 -0400

    Update copyprog.sh
    Just added a quick directory check.

--- copyprog.sh
+++ copyprog.sh
@@ -2,10 +2,20 @@
 # $1 = directory of files to copy, $2 directory to copy into
 # Please install the "pv" program and make sure you have access to both directories
 
+if [ $# -eq 0 ]
+then
+    echo "No arguments supplied"
+    exit
+fi
+
+pwd=$(pwd)
 b=$(du -s -B1 $1 | awk -F " " '{print $1}')
 echo $b
 
-mkdir $2
+if [ ! -d "$2" ]
+then
+    mkdir $2
+fi
 
 cd $1
 tar -c . | pv -s $b | tar -x -C $2
