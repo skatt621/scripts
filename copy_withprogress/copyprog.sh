f1c3c75e1444ef227866eed18e79631d8f160a72
Sandeep Kattepogu <sandeep@kattepogu.com>
2020-09-26 14:56:00 -0400

    small changes for stopping errors

--- copy_withprogress/copyprog.sh
+++ copy_withprogress/copyprog.sh
@@ -10,19 +10,19 @@ fi
 
 if [[ $# -gt 3 ]]
 then
-    echo "Please supply ONLY two folder locations: folder to copy from and folder to copy to."
+    echo "Please supply ONLY two folder locations: folder/file to copy from and folder to copy to."
     exit
 fi
 
 # Source/target checks
-ls "$1"
+ls "$(realpath $1)" > /dev/null 2>&1
 if [[ $? -gt 0 ]]
 then
     echo "The source files/directory { $1 } does not exist."
     exit
 fi
 
-ls "$2"
+ls "$(realpath $1)" > /dev/null 2>&1
 if [[ $? -gt 0 ]] || [[ -f "$2" ]]
 then
     echo "The target directory { $2 } does not exist (or is really a file)."
@@ -32,12 +32,15 @@ fi
 # Settings byte numbers for pv
 b=$(du -s -b "$1" | awk -F " " '{print $1}')
 echo $b
+b_human=$(du -s -h "$1" | awk -F " " '{print $1}')
+echo $b_human
 
 # Compressing and transporting a folder's directories
 if [[ -d "$1" ]]
 then
+    src=$(realpath $1)
     targ=$(realpath $2)
-    cd "$1"
+    cd "$src"
     tar -c . | pv -s $b | tar -x -C $targ
     cd -
 fi
